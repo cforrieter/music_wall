225e7a39f6ff0dcff06d4cbbc1afc51241b8ea0e
cforrieter <cforrieter@gmail.com>
2015-09-21 20:09:38 +0000

    Add new song creation. Validate for youtube links. Embed youtube videos based on link provided. Add materializecss for design

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,19 @@
 # Homepage (Root path)
 get '/' do
+  @songs = Song.all
   erb :index
+end
+
+get '/new' do
+  @song = Song.new
+  erb :new
+end
+
+post '/' do
+  @song = Song.new(name: params[:name], author: params[:author], url: params[:url])
+  if @song.save
+    redirect '/'
+  else
+    erb :new
+  end
 end
---
+++ app/models/song.rb
@@ -0,0 +1,11 @@
+class Song < ActiveRecord::Base
+  validates :name, :author, presence: true
+  validates :url, format: { with: /https?:\/\/(?:www)?\.?youtube/, message: "must be link from YouTube."}
+  validate :youtube_validation
+
+  def youtube_validation
+    unless url.empty?
+      self.url = /https?:\/\/(?:www)?\.?youtube\.\w+\/\w+\?\w+=(\w+)/.match(url).captures[0]
+    end
+  end
+end
